63fe715276fafc6ea7d64c88458fc91af2eb6cef
Pieter-Jan Caals <pieter.jan.caals@gmail.com>
2025-04-24 09:45:32 +0200

    Split aliases in another file

--- .pwsh/profile.ps1
+++ .pwsh/profile.ps1
@@ -1,102 +1,8 @@
-# General
-function Set-Profile {
-    [Alias("rl")]
-    [Alias("source")]
-    param()
-    . C:\Users\pcaals\.dotfiles\.pwsh\profile.ps1
-}
-
-# GIT
-function Get-GitStatus {
-    [Alias("gs")]
-    param()
-    git status
-}
-
-function gd {
-    git diff
-}
-
-if ((Get-AliasCommand gp -ne "Push-Git")) {
-    Remove-Alias gp -Force
-    Set-Alias gp Push-Git
-}
-function Push-Git {
-    [Alias("gp")]
-    param()
-
-    git push
-}
-
-function gpu {
-    param (
-        $Branch
-    )
-
-    git push -u origin $Branch
-}
-
-function gac {
-    param (
-        $Message
-    )
-
-    git add . ; git commit -m $Message
-}
-
-# Movement
-function goto {
-    [Alias("gt")]
-    param (
-        $location
-    )
-
-    Switch ($location) {
-        "devo" {
-            Set-Location -Path "$HOME/devoteam"
-        }
-        "pr" {
-            Set-Location -Path "$HOME/01-projects"
-        }
-        "dot" {
-            Set-Location -Path "$HOME/.dotfiles"
-        }
-        default {
-            Write-Host "Invalid location"
-        }
-    }
-}
-
-if (Get-AliasCommand ls -ne "Get-Files") {
-    Remove-Alias ls
-    Set-Alias ls Get-Files
-}
-
-function Get-Files {
-    param(
-        [string]$Path = "."
-    )
-
-    eza.exe --group-directories-first --git --icons $Path
-}
-
-function ll {
-    param(
-        [string]$Path = "."
-    )
-
-    eza.exe --group-directories-first -l --git --icons $Path
-}
-
-function la {
-    param(
-        [string]$Path = "."
-    )
-
-    eza.exe -lab --group-directories-first -l --git --icons $Path
-}
+# Load aliases
+. $HOME\.dotfiles\.pwsh\aliases.ps1
 
 # Enable tab completion for AZ CLI
+
 Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
     $completion_file = New-TemporaryFile
@@ -117,23 +23,6 @@ Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-
-# Helper Functions
-function Get-AliasCommand {
-    param(
-        $Command
-    )
-
-    try {
-        $alias = Get-Alias $Command
-        return ($alias | Select-Object -Property ResolvedCommandName).ResolvedCommandName
-    }
-    catch {
-        return ""
-    }
-
-}
-
-# $ENV:STARSHIP_CONFIG = "$HOME\.dotfiles\.config\starship.toml"
-# $ENV:STARSHIP_DISTRO = "者 xcad"
-# Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\.dotfiles\.config\starship.toml"
+$ENV:STARSHIP_DISTRO = "者 xcad"
+Invoke-Expression (&starship init powershell)
